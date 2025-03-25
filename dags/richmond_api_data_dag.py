eca10fb54dbc6a3a736c734d769876104261e700
luuisss1598 <39506329+luuisss1598@users.noreply.github.com>
2025-03-24 23:40:16 -0700

    chore: create body of dags and design

--- dags/richmond_api_data_dag.py
+++ dags/richmond_api_data_dag.py
@@ -2,6 +2,10 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.empty import EmptyOperator
 from datetime import datetime, timedelta
+import requests
+import pandas as pd
+import uuid
+import os
 
 default_args = {
     "owner": "Airflow",
@@ -13,6 +17,69 @@ default_args = {
     "retry_delay": timedelta(minutes=5)  # Fixed typo here (removed the line break)
 }
 
+API_ENDPOINTS = {
+    'richmond_water_consumption_data': 'https://www.transparentrichmond.org/resource/xivi-n6g4.json',
+    'water_consumption': 'https://www.transparentrichmond.org/resource/98sv-3bgz.json',
+    'single_family_water_consumption': 'https://www.transparentrichmond.org/resource/5wpn-bryw.json',
+    'multi_family_water_consumption': 'https://www.transparentrichmond.org/resource/8dqm-egfs.json',
+    'municipal_family_utility_usage': 'https://www.transparentrichmond.org/resource/9v6a-54wj.json'
+}
+
+RAW_DATA_SOURCE_PATH: str = './data/raw/water_quality/water_quality_lab_results.csv'
+TEMP_STORAGE_PATH: str = './data/processed/richmond_catalog_api'
+SCHEMA_NAME: str = 'water_quality'
+
+# make sure temp file exist
+os.makedirs(TEMP_STORAGE_PATH, exist_ok=True)
+
+def extract_richmond_catalog_api(api_endpoint: str, **context):
+    run_id = str(uuid.uuid4()) # create unique_id for temp json file
+    run_id_file_path = os.path.join(TEMP_STORAGE_PATH, f'run_{run_id}.json')
+    
+    response = requests.get(url=api_endpoint, timeout=5)
+    data = response.json()
+    
+    # create dataframe
+    df = pd.DataFrame(data)
+    
+    # create the file, the return all metadata for file
+    df.to_json(run_id_file_path,index=False)
+    
+    metadata_api = {
+        'run_id': run_id,
+        'file_path': run_id_file_path
+    }
+    
+    return metadata_api
+
+def transform_richmond_catalog_api(**context):
+    ti = context['ti']
+    
+    t1 = ti.xcom_pull(task_ids='extract_water_consumption')
+    t2 = ti.xcom_pull(task_ids='extract_single_family_water_consumption')
+    t3 = ti.xcom_pull(task_ids='extract_richmond_water_consumption_data')
+    t4 = ti.xcom_pull(task_ids='extract_municipal_family_utility_usage')
+    t5 = ti.xcom_pull(task_ids='extract_multi_family_water_consumption')
+    
+    
+    extract_metadata = [t1, t2, t3, t4, t5]
+    
+    return {
+        'extract_water_consumption': t1,
+        'extract_single_family_water_consumption': t2,
+        'extract_richmond_water_consumption_data': t3,
+        'extract_municipal_family_utility_usage': t4,
+        'extract_multi_family_water_consumption': t5,
+    }
+    
+def load_richmond_catalog_api(task_id: str, **context):
+    ti = context['ti']
+    transformed_metadata = ti.xcom_pull(task_ids='transform_all_data_sources')
+    
+    return {
+        'task_id': task_id
+    }
+
 with DAG(
     'ingest_richmond_api_data',
     default_args=default_args,
@@ -23,37 +90,73 @@ with DAG(
     tags=['water', 'api']
 ) as dag:
     
-    task_1 = EmptyOperator(
-        task_id='extract_endpoint_1',
+    water_consumption_task = PythonOperator(
+        task_id='extract_water_consumption',
+        python_callable=extract_richmond_catalog_api,
+        op_kwargs={
+            'api_endpoint': API_ENDPOINTS['water_consumption']
+        }
     )
 
-    task_2 = EmptyOperator(
-        task_id='extract_endpoint_2'
+    single_family_water_consumption_task = PythonOperator(
+        task_id='extract_single_family_water_consumption',
+        python_callable=extract_richmond_catalog_api,
+        op_kwargs={
+            'api_endpoint': API_ENDPOINTS['single_family_water_consumption']
+        }
     )
 
-    task_3 = EmptyOperator(
-        task_id='extract_endpoint_3',
+    richmond_water_consumption_data_task = PythonOperator(
+        task_id='extract_richmond_water_consumption_data',
+        python_callable=extract_richmond_catalog_api,
+        op_kwargs={
+            'api_endpoint': API_ENDPOINTS['richmond_water_consumption_data']
+        }
     )
 
-    task_4 = EmptyOperator(
-        task_id='transform_all_data',
+    municipal_family_utility_usage_task = PythonOperator(
+        task_id='extract_municipal_family_utility_usage',
+        python_callable=extract_richmond_catalog_api,
+        op_kwargs={
+            'api_endpoint': API_ENDPOINTS['municipal_family_utility_usage']
+        }
     )
 
-    task_5 = EmptyOperator(
-        task_id='load_endpoint_1',
+    multi_family_water_consumption_task = PythonOperator(
+        task_id='extract_multi_family_water_consumption',
+        python_callable=extract_richmond_catalog_api,
+        op_kwargs={
+            'api_endpoint': API_ENDPOINTS['multi_family_water_consumption']
+        }
     )
     
-    task_6 = EmptyOperator(
-        task_id='load_endpoint_2',
+    transform_all_data_sources_task = PythonOperator(
+        task_id='transform_all_data_sources',
+        python_callable=transform_richmond_catalog_api
     )
 
-    task_7 = EmptyOperator(
-        task_id='load_endpoint_3',
+    load_water_consumption_task = PythonOperator(
+        task_id='load_water_consumption',
+        python_callable=load_richmond_catalog_api,
+        op_kwargs={
+            'task_id': 'extract_water_consumption'
+        }
     )
 
-    task_8 = EmptyOperator(
-        task_id='clean_up_all_files',
+    load_single_family_water_consumption_task = PythonOperator(
+        task_id='load_single_family_water_consumption',
+        python_callable=load_richmond_catalog_api,
+        op_kwargs={
+            'task_id': 'extract_single_family_water_consumption'
+        }
     )
 
 
-    [task_1, task_2, task_3] >> task_4 >> [task_5, task_6, task_7] >> task_8
+    [
+        water_consumption_task, single_family_water_consumption_task, 
+        richmond_water_consumption_data_task, municipal_family_utility_usage_task, 
+        multi_family_water_consumption_task
+    ] >> transform_all_data_sources_task >> [
+        load_water_consumption_task, 
+        load_single_family_water_consumption_task
+    ]
